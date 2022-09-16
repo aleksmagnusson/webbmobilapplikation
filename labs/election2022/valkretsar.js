e2e4edee463b63ad44f415eebc45435d4972056a
aleksmagnusson <aleks.magnusson@gmail.com>
2022-09-16 15:09:39 +0200

    plugga mer på grunderna [x]

--- labs/election2022/valkretsar.js
+++ labs/election2022/valkretsar.js
@@ -1,7 +1,55 @@
 const valData = require("./data.json");
+// Shorten data from './data.json' to a better variable name.
+const valKretsar = valData.valkretsar;
 
-function getVotePercentage() {
-  console.log(`Valdeltagande 2022: ${valData.valdeltagande}`);
+function listOfDistricts() {
+  // .map skapar en ny array för varje item.
+  const distrikt = valKretsar.map((distrikt) => {
+    return `${distrikt.namn}`;
+  });
+
+  console.log(`Alla valkretsar 2022: ${distrikt.join(", ")}`);
 }
 
-getVotePercentage();
+listOfDistricts();
+
+/**
+ * Argument: districtName
+ * Task: From each district, create a function that brings amout of votes for each party.
+ * (valkretsar.namn) => (partiforkortning): (antalRoster) + (andelRoster) =
+ * "Södermanlands län" => "M: 34 407 (19,2%)"...
+ */
+
+function districtResults(districtName) {
+  valKretsar.forEach((distrikt) => {
+    if (districtName.toLowerCase() === distrikt.namn.toLowerCase()) {
+      const partirost = distrikt.rosterPaverkaMandat.partiroster;
+      console.log(districtName);
+      partirost.forEach((party) => {
+        console.log(
+          `${party.partiforkortning}: ${party.antalRoster} (${party.andelRoster})`
+        );
+      });
+    }
+  });
+  return;
+}
+
+districtResults("Stockholms kommun");
+
+function partyResults(party) {
+  valKretsar.forEach((rosterResultat) => {
+    const roster = rosterResultat.rosterPaverkaMandat.partiroster;
+
+    roster.forEach((rostPerParti) => {
+      if (party === rostPerParti.partiforkortning) {
+        console.log(
+          `${rosterResultat.namn}: ${rosterResultat.antalRoster} (${rosterResultat.andelRoster})`
+        );
+      }
+    });
+  });
+  return;
+}
+
+districtResults("S");
